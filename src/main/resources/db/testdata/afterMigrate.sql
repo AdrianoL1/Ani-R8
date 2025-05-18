029f00995147b0a96ddc3f90ad0d527b3efb93d3
Holanddez <elfosupremo@hotmail.com>
2025-05-18 17:46:12 -0300

    raw: add admin credentials

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -16,6 +16,8 @@ ALTER TABLE manga auto_increment = 1;
 ALTER TABLE genre auto_increment = 1;
 ALTER TABLE anime_genre auto_increment = 1;
 ALTER TABLE manga_genre auto_increment = 1;
+ALTER TABLE user auto_increment = 1;
+ALTER TABLE role auto_increment = 1;
 
 INSERT INTO `anime` (`aired_from`, `aired_to`, `author`, `description`, `status`, `title`, `total_episodes`) VALUES ('2004','2005','Naoki Urasawa','Monster anime','FINISHED','Monster','74');
 INSERT INTO `genre` (`name`) VALUES ('Fantasy'),('Action'),('Romance');
@@ -23,3 +25,5 @@ INSERT INTO `manga` (`author`, `chapters`, `description`, `published_from`, `pub
 INSERT INTO `anime_genre` (`anime_id`, `genre_id`) VALUES (1,1),(1,2);
 INSERT INTO `manga_genre` (`manga_id`, `genre_id`) VALUES (1,1),(1,2);
 INSERT INTO `role` (`name`) VALUES ('ADMIN'), ('USER');
+INSERT INTO `user` (`email`, `password`, `username`) VALUES ('admin@admin.com', '$2a$12$OPQdHPI7xWobP3AQGDLkiOgVSwaIcnDGAvcj0KHlIcKEvC1gV2Aca', 'sysAdm');
+INSERT INTO `user_role` VALUES (1, 1)
